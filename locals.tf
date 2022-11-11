4458019ed75ca484c5503d4c0c63be15fc58cf30
Mike Ball <mikedball@gmail.com>
2022-11-11 06:08:54 -0500

    folder named by git repo

--- locals.tf
+++ locals.tf
@@ -10,9 +10,9 @@ locals {
   # collect a list of objects in the format of...
   # {
   #   repo        = repository-name
-  #   folder_name = mdb_repository-name_software-component-name
+  #   folder_name = github-owner_repository-name_microservice
   # }
-  # ...where `software-component-name` is the directory name in which a Dockerfile lives.
+  # ...where `microservice` is the directory name in which a Dockerfile lives.
   # For example, the following directory structure homes 'foo' and 'bar' container images:
   # repository-name/
   #   foo/Dockerfile
@@ -21,7 +21,7 @@ locals {
     for repo_tree in data.github_tree.all : [
       for entry in repo_tree.entries : {
         repo        = repo_tree.repository
-        folder_name = try("${local.owner}_${replace(repo_tree.repository, ".", "")}_${split("/", entry.path)[length(split("/", entry.path)) - 2]}", "${local.owner}_${replace(repo_tree.repository, ".", "")}_root")
+        folder_name = try("${local.owner}_${replace(repo_tree.repository, ".", "")}_${split("/", entry.path)[length(split("/", entry.path)) - 2]}", "${local.owner}_${replace(repo_tree.repository, ".", "")}")
       } if endswith(entry.path, "Dockerfile")
     ]
   ]))
